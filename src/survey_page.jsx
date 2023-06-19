70ca5ee260397e5c93733ad9c9cda039b79ec3fd
Jigneshmotwani <96459160+Jigneshmotwani@users.noreply.github.com>
2023-06-20 00:43:00 +0800

    1

---
+++ src/survey_page.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+
+const SurveyPage = () => {
+  return (
+    <>
+      <div
+        className="banner"
+        style={{
+          backgroundImage: `url(${process.env.PUBLIC_URL}/Images/Banner1.jpeg)`,
+          backgroundPosition: "top center",
+          height: "420px",
+          backgroundSize: "cover",
+          backgroundRepeat: "no-repeat",
+          position: "relative",
+          marginTop: "-1px",
+        }}
+      >
+        <center>
+          <h4>VIETNAM</h4>
+          <br />
+          <h2>Hanoi</h2>
+        </center>
+      </div>
+      <div className="TileContainer"></div>
+    </>
+  );
+};
+
+export default SurveyPage;
