566400d9d48aeb05a9d7027c715857db666ded05
Sarang Nambiar <sarangnambiar455@gmail.com>
2023-06-21 20:36:49 +0800

    Header

--- src/App.js
+++ src/App.js
@@ -4,21 +4,23 @@ import Header from './Header.js';
 
 function App() {
   return (
-    <div className="App">
+    <>
       <Header />
-      <img src={logo} className="App-logo" alt="logo" />
-      <p>
-        Edit <code>src/App.js</code> and save to reload.
-      </p>
-      <a
-        className="App-link"
-        href="https://reactjs.org"
-        target="_blank"
-        rel="noopener noreferrer"
-      >
-        Learn React
-      </a>
-    </div>
+      <div className="App">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </div>
+    </>
   );
 }
 
