85229ce8a9ae48fc4864084c01fa89ceef76d99a
Sarang Nambiar <sarangnambiar455@gmail.com>
2023-06-21 17:16:41 +0800

    Header added

--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import SurveyPage from './survey_page.jsx';
+import App from './App.js';
 
 ReactDOM.render(
   <React.StrictMode>
+        <App/>
     <SurveyPage />
   </React.StrictMode>,
   document.getElementById('root')
